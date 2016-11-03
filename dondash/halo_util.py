c59b5cea109ec634e179c56b96c001320ec8450f
jgibbons-cp <jgibbons@cloudpassage.com>
2016-11-02 20:06:59 -0700

    working on fim

--- dondash/halo_util.py
+++ dondash/halo_util.py
@@ -46,22 +46,22 @@ class SecurityReporter(object):
         scan_module = cloudpassage.Scan(self.halo_session)
         raw_scan_results = []
         # Initiate scans
-        print "Initiating scans for agent %s" % agent_id
+        #print "Initiating scans for agent %s" % agent_id
         for scan_type in scan_types:
             command_id = scan_module.initiate_scan(agent_id, scan_type)["id"]
             command_ids.append(command_id)
-            print command_id
+            #print command_id
         # Wait until all are complete
-        print "Waiting for all scan jobs to be run"
+        #print "Waiting for all scan jobs to be run"
         while len(command_ids) > 0:
             time.sleep(30)
             for command_id in command_ids:
-                print "Checking status of command %s" % command_id
+                #print "Checking status of command %s" % command_id
                 status = server_module.command_details(agent_id, command_id)
                 if status not in unfinished_statuses:
                     command_ids.remove(command_id)
         # Get results
-        print "Getting scan results"
+        #print "Getting scan results"
         for scan_type in scan_types:
             try:
                 results = scan_module.last_scan_results(agent_id, scan_type)
