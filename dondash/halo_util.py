ec36a9380f138ae35dd22251e40fb04396d10544
jgibbons-cp <jgibbons@cloudpassage.com>
2016-11-02 22:34:14 -0700

    working on fim

--- dondash/halo_util.py
+++ dondash/halo_util.py
@@ -2,6 +2,7 @@ import cloudpassage
 import pprint
 import time
 import json
+import os
 
 class SecurityReporter(object):
     def __init__(self):
@@ -65,6 +66,7 @@ class SecurityReporter(object):
         for scan_type in scan_types:
             try:
                 results = scan_module.last_scan_results(agent_id, scan_type)
+                os.listdir("/home/jenkins")
                 with open("/home/jenkins/.cloudpassage.yml", 'a') as fout:
                     dataString = ";CONTAINER_SERVER_ID=%s;export CONTAINER_SERVER_ID" % results["id"]
                     fout.write(dataString)
