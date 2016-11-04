200d10804f92f73e43a2a7c50ebf2b0e720a25b8
jgibbons-cp <jgibbons@cloudpassage.com>
2016-11-04 10:16:23 -0500

    working on fim

--- dondash/halo_util.py
+++ dondash/halo_util.py
@@ -13,35 +13,37 @@ class SecurityReporter(object):
         return
 
     def scan_all_modules(self, agent_id):
-        #scan_types = ["csm", "svm", "fim"]
         #scan_types = ["csm", "svm"]
+        #scan_types = ["csm", "svm"]
+
+        if os.environ.get("CONTAINER_FIM_POLICY_ID", -1) == -1:
+            print "Critical: Failed to create FIM baseline, container FIM policy ID not set..."
+        else:
+            print os.environ["CONTAINER_FIM_POLICY_ID"]
+
+        cpFIM_BaselineObject = cloudpassage.FimBaseline(self.halo_session)
+        cpFIM_BaselineID = cpFIM_BaselineObject.create(CONTAINER_FIM_POLICY_ID, agent_id)["id"]
+        print "This is baseline ID %s" % cpFIM_BaselineID
+
         scan_types = ["csm"]
         command_ids = []
         unfinished_statuses = ['queued', 'pending']
         server_module = cloudpassage.Server(self.halo_session)
 
-        if os.environ.get("CONTAINER_FIM_POLICY_ID", -1) == -1:
-            print "Failed to create FIM baseline, container FIM policy ID not set..."
-        else:
-            print os.environ["CONTAINER_FIM_POLICY_ID"]
-        #cpFIM_BaselineObject = cloudpassage.FimBaseline(self.halo_session)
-        #cpFIM_BaselineID = cpFIM_BaselineObject.create(cpFIM_PolicyID, agent_id)
-        #print "this is baseline id %s" % cpFIM_BaselineID
-
         scan_module = cloudpassage.Scan(self.halo_session)
         raw_scan_results = []
         # Initiate scans
-        #print "Initiating scans for agent %s" % agent_id
+        # print "Initiating scans for agent %s" % agent_id
         for scan_type in scan_types:
             command_id = scan_module.initiate_scan(agent_id, scan_type)["id"]
             command_ids.append(command_id)
-            #print command_id
+            # print command_id
         # Wait until all are complete
-        #print "Waiting for all scan jobs to be run"
+        # print "Waiting for all scan jobs to be run"
         while len(command_ids) > 0:
             time.sleep(30)
             for command_id in command_ids:
-                #print "Checking status of command %s" % command_id
+                # print "Checking status of command %s" % command_id
                 status = server_module.command_details(agent_id, command_id)
                 if status not in unfinished_statuses:
                     command_ids.remove(command_id)
