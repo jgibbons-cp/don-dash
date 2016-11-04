71b7cb874c2b0fb121b46e6522bbddfc14bf42f4
jgibbons-cp <jgibbons@cloudpassage.com>
2016-11-04 15:07:10 -0500

    point to create baseline and assess script

--- create_fim_base_and_scan.sh
+++ create_fim_base_and_scan.sh
@@ -5,4 +5,4 @@ echo "We'll sleep for 10s to give the agent a chance to authenticate"
 sleep 10
 export AGENT_ID=`cat /opt/cloudpassage/data/id`
 
-python /app/self_assessment.py
+python /app/create_fim_base_and_self_assess.py
