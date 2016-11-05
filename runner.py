6364b905a54453ecaa6633533a06c00b4d847f8f
jgibbons-cp <jgibbons@cloudpassage.com>
2016-11-05 17:45:24 -0500

    working on fim

--- runner.py
+++ runner.py
@@ -11,7 +11,7 @@ halo_session = cloudpassage.HaloSession(halo_creds.key_id, halo_creds.secret_key
 
 def fimScan(halo_session):
     agent_id = os.getenv("AGENT_ID")
-    scan_type = "fim"
+    scan_types = scan_types = ["fim"]
     #scan_object = cloudpassage.Scan(halo_session)
     #scan_id = scan_object.initiate_scan(agent_id, scan_type)
     #results = scan_object.last_scan_results(agent_id, scan_type)
