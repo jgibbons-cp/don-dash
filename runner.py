61a4611e178987f936d1b4c63f919b0340a7b9ab
jgibbons-cp <jgibbons@cloudpassage.com>
2016-11-05 16:27:51 -0500

    working on fim

--- runner.py
+++ runner.py
@@ -14,13 +14,13 @@ def get_servers(halo_session):
     all_servers = servers_object.list_all()
     return all_servers
 
-def fimScan(halo_session):
-    agent_id = os.getenv("AGENT_ID")
-    scan_type = "fim"
-    scan_object = cloudpassage.Scan(halo_session)
-    scan_id = scan_object.initiate_scan(agent_id, scan_type)
-    results = last_scan_results(agent_id, scan_type)
-    return results
+#def fimScan(halo_session):
+#    agent_id = os.getenv("AGENT_ID")
+#    scan_type = "fim"
+#    scan_object = cloudpassage.Scan(halo_session)
+#    scan_id = scan_object.initiate_scan(agent_id, scan_type)
+#    results = last_scan_results(agent_id, scan_type)
+#    return results
 
 @app.route('/')
 def home_page():
