9a4f4a61716a273c3fc9f3258ab2079d2724892e
jgibbons-cp <jgibbons@cloudpassage.com>
2016-11-05 16:48:18 -0500

    working on fim

--- run_with_halo.sh
+++ run_with_halo.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+source /home/core/.cloudpassage.yml
+
 /opt/cloudpassage/bin/cphalo --agent-key=$HALO_AGENT_KEY --tag=$SERVER_GROUP --server-label="don-dash" 2>&1 >/dev/null &
 
 sleep 10
